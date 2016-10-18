ec5697129e5a22c2d165331344d65edaf62c6cab
admin@infomatrix.com.au <admin@infomatrix.com.au>
2016-10-19 01:14:04 +1000

    Setup Web API project

---
+++ src/MyWebApp/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace MyWebApp.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class HealthController : ApiController
+    {
+        [HttpGet]
+        [Route("{id:int}")]
+        public string Get(int id)
+        {
+            return $"Your number is {id}";
+        }
+    }
+}
